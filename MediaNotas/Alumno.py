bb6e422e8cd0520a1f0b51d5848177e0f392c2b6
jojoseja <jfeyjoo@gmail.com>
2025-10-09 22:22:24 +0200

    hola

--- MediaNotas/Alumno.py
+++ MediaNotas/Alumno.py
@@ -1,12 +1,25 @@
 class Alumno:
-    def __init__(self, nombre, apellidos, mates, biologia, ingles, lengua):
+    contador = 1
+
+    def __init__(self, nombre, apellidos, py, dm, ds, calc, oop, java, cplus, db, webdev, ai):
+        self.id = Alumno.contador
         self.nombre = nombre
         self.apellidos = apellidos
-        self.mates = mates
-        self.biologia = biologia
-        self.ingles = ingles
-        self.lengua = lengua
+        self.py = py
+        self.dm = dm
+        self.ds = ds
+        self.calc = calc
+        self.oop = oop
+        self.java = java
+        self.cplus = cplus
+        self.db = db
+        self.webdev = webdev
+        self.ai = ai
+        Alumno.contador += 1
 
     def to_dict(self):
-        return {"Nombre": self.nombre, "Apellidos": self.apellidos, "Mates": self.mates, "Biología": self.biologia,
-                "Inglés": self.ingles, "Lengua": self.lengua}
+        return {"ID": self.id, "Nombre": self.nombre, "Apellidos": self.apellidos, "Python": self.py,
+                "Discrete Mathematics": self.dm, "Data Structures": self.ds, "Calculus": self.calc,
+                "Object-Oriented Programming": self.oop, "Java": self.java, "C++": self.cplus, "Databases": self.db,
+                "Web Development": self.webdev, "AI": self.ai
+                }
--- MediaNotas/p1.py
+++ MediaNotas/p1.py
@@ -14,34 +14,43 @@ apellidos = ["Alvarez", "Blanco", "Castillo", "Díaz", "Estévez", "Fernandez", 
 
 
 
-
+#nombre, apellidos, py, dm, ds, calc, oop, java, cplus, db, webdev, ai)
 def escribirCSV():
     data = []
-    for i in range(10000):
+    for i in range(1000000):
         data.append(Alumno(rand.choice(nombres), rand.choice(apellidos),
-                           rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100),
+                           rand.randint(0, 100),rand.randint(0, 100), rand.randint(0, 100),
+                           rand.randint(0, 100),rand.randint(0, 100),rand.randint(0, 100),
+                           rand.randint(0, 100),rand.randint(0, 100),rand.randint(0, 100),
                            rand.randint(0, 100)).to_dict())
     with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Nombre', 'Apellidos', 'Mates', 'Biología', 'Inglés', 'Lengua']
+        fieldnames = ['ID', 'Nombre', 'Apellidos', 'Python', 'Discrete Mathematics', 'Data Structures', 'Calculus',
+                      'Object-Oriented Programming', 'Java', 'C++', 'Databases', 'Web Development', 'AI']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
 
 #escribirCSV()
 
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 df = pd.read_csv('data.csv') # Read csv as a datafile
 
 
-non_grade_columns=["Nombre", "Apellidos"]
+non_grade_columns=["ID", "Nombre", "Apellidos"]
 grade_columns= [col for col in df.columns if col not in non_grade_columns]
 
 df['Average Score'] = df[grade_columns].mean(axis=1)
-#print(df.sort_values(by=['Average Score'], ascending=False).head(20))
-print(df)
+
+sorted_by_average_score = df.sort_values(by=['Average Score'], ascending=False).head(3)
+print(sorted_by_average_score)
 
 
-#filtered_students = df[(df[grade_columns] > 40).all(axis=1)]
-#filtered_students_sorted = filtered_students.sort_values(by=['Mates'], ascending=False)
+filtered_students = df[(df[grade_columns] > 60).all(axis=1)]
+filtered_students_sorted = filtered_students.sort_values(by=['Average Score'], ascending=False)
+#print(filtered_students_sorted)
 
 #df['average_score'] = df[grade_columns].mean(axis=1)
 
